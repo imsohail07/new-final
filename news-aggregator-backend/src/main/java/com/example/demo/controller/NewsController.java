4d2c09d64fc4c2d4fa2250b805cef73fac655a8b
Sohail Shirazi <shirazisohail.md@gmail.com>
2026-01-16 23:00:41 +0530

    few change

--- news-aggregator-backend/src/main/java/com/example/demo/controller/NewsController.java
+++ news-aggregator-backend/src/main/java/com/example/demo/controller/NewsController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/news")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class NewsController {
 
     private final NewsService newsService;
